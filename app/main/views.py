b390610969cf177e60dcbc03e18b47d9fb2665b1
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-12 09:07:21 +0300

    Added styling to the index page and added a side bar

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,32 @@
+# from turtle import title
+from email import message
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 # from .forms import exampleForm
 from flask_login import login_required, current_user
 
+posts = [
+    {
+        'author': 'Amani',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 12, 2022'
+    },
+    {
+        'author': 'Joe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 12, 2022'
+    }
+]
 
 @main.route('/')
 def index():
     message = 'Hello Amani'
-    return render_template('index.html', message=message)
+    return render_template('index.html', message=message, posts=posts)
 
 
 @main.route('/about/')
 def about():
-    message = 'About page'
-    return render_template('about.html', message=message)
+    message = "This is the about page"
+    return render_template('about.html', title='About', message=message)
