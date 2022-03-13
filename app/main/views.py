19e921e368e41e7d62db77c5ec5db5a2e27b270f
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-13 16:42:10 +0300

    Working dilike and like buttons for logged in users

--- app/main/views.py
+++ app/main/views.py
@@ -38,16 +38,16 @@ def about():
 @login_required
 def comment(blog_id):
     form = CommentForm()
-    post = Blog.query.get(blog_id)
+    blog = Blog.query.get(blog_id)
     all_comments = Comment.query.filter_by(blog_id = blog_id).all()
     if form.validate_on_submit():
         comment = form.comment.data 
-        post_id = post_id
+        blog_id = blog_id
         user_id = current_user._get_current_object().id
         new_comment = Comment(comment = comment,user_id = user_id,blog_id = blog_id)
         new_comment.save_c()
         return redirect(url_for('.comment', blog_id = blog_id))
-    return render_template('comment.html', form =form, post = post,all_comments=all_comments)
+    return render_template('comment.html', form =form, blog = blog,all_comments=all_comments)
 
 
 
@@ -106,31 +106,31 @@ def update_pic(name):
 @main.route('/like/<int:id>',methods = ['POST','GET'])
 @login_required
 def like(id):
-    get_pitches = Upvote.get_upvotes(id)
+    get_blogs = Upvote.get_upvotes(id)
     valid_string = f'{current_user.id}:{id}'
-    for pitch in get_pitches:
-        to_str = f'{pitch}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_vote = Upvote(user = current_user, pitch_id=id)
-    new_vote.save()
-    return redirect(url_for('main.index',id=id))
-
-@main.route('/dislike/<int:id>',methods = ['POST','GET'])
-@login_required
-def dislike(id):
-    pitch = Downvote.get_downvotes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for p in pitch:
+    for p in get_blogs:
         to_str = f'{p}'
         print(valid_string+" "+to_str)
         if valid_string == to_str:
             return redirect(url_for('main.index',id=id))
         else:
             continue
-    new_downvote = Downvote(user = current_user, pitch_id=id)
+    new_vote = Upvote(user = current_user, blog_id=id)
+    new_vote.save()
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    blog = Downvote.get_downvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for p in blog:
+        to_str = f'{p}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_downvote = Downvote(user = current_user, blog_id=id)
     new_downvote.save()
     return redirect(url_for('main.index',id = id))
